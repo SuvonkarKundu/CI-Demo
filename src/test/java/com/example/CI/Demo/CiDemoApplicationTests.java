3170a71108c7cc903c508d7d9070612cb4d31dcd
Suvonkar Kundu <mailto:suvonkar.kundu@valmetpartners.com>
2024-08-29 13:23:20 +0600

    added logger

---
+++ src/test/java/com/example/CI/Demo/CiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CI.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
